ed501e21bd118bf90971cc2b093187af20dcf664
Kai <8343141+Mik4sa@users.noreply.github.com>
2021-03-30 00:05:04 +0200

    Renamed icon file

--- AssemblyInformation.Launcher/Add Shell Extension.ps1
+++ AssemblyInformation.Launcher/Add Shell Extension.ps1
@@ -1,7 +1,7 @@
 function Add-ShellExtension($fileExtension) {
 	$key = Get-ItemPropertyValue "Registry::HKCR\.$fileExtension" "(default)"
 	New-Item -Path "Registry::HKCU\SOFTWARE\Classes\$key\shell\AssemblyInformation\command" -Value """$PSScriptRoot\AssemblyInformation.Launcher.exe"" ""%1""" -Force
-	New-ItemProperty -Path "Registry::HKCU\SOFTWARE\Classes\$key\shell\AssemblyInformation" -Name "icon" -Value """$PSScriptRoot\Icon.ico""" -Force
+	New-ItemProperty -Path "Registry::HKCU\SOFTWARE\Classes\$key\shell\AssemblyInformation" -Name "icon" -Value """$PSScriptRoot\AssemblyInformation.ico""" -Force
 }
 
 Add-ShellExtension("dll")
